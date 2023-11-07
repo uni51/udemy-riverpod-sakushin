ba0d1cb151f12919a85484f10c172213577dcfe3
uni51 <unieye51@gmail.com>
2023-11-07 14:39:37 +0900

    28. Modelのロジックをテスト：リセット

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -38,5 +38,20 @@ void main() {
     expect(target.countData.countUp, 0);
     expect(target.countData.countDown, 4);
   });
-  test('reset', ()async{});
+  test('reset', ()async{
+    expect(target.countData.count, 0);
+    expect(target.countData.countUp, 0);
+    expect(target.countData.countDown, 0);
+    target.increase();
+    target.increase();
+    target.decrease();
+    expect(target.countData.count, 1);
+    expect(target.countData.countUp, 2);
+    expect(target.countData.countDown, 1);
+
+    target.reset();
+    expect(target.countData.count, 0);
+    expect(target.countData.countUp, 0);
+    expect(target.countData.countDown, 0);
+  });
 }
