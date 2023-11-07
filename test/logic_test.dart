545db458699106221484052e6380044c185c9f31
uni51 <unieye51@gmail.com>
2023-11-07 14:36:38 +0900

    27. Modelのロジックをテスト：メソッド

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -12,7 +12,31 @@ void main() {
     expect(target.countData.countUp, 0);
     expect(target.countData.countDown, 0);
   });
-  test('increase', ()async{});
-  test('decrease', ()async{});
+  test('increase', ()async{
+    target.increase();
+    expect(target.countData.count, 1);
+    expect(target.countData.countUp, 1);
+    expect(target.countData.countDown, 0);
+
+    target.increase();
+    target.increase();
+    target.increase();
+    expect(target.countData.count, 4);
+    expect(target.countData.countUp, 4);
+    expect(target.countData.countDown, 0);
+  });
+  test('decrease', ()async{
+    target.decrease();
+    expect(target.countData.count, -1);
+    expect(target.countData.countUp, 0);
+    expect(target.countData.countDown, 1);
+
+    target.decrease();
+    target.decrease();
+    target.decrease();
+    expect(target.countData.count, -4);
+    expect(target.countData.countUp, 0);
+    expect(target.countData.countDown, 4);
+  });
   test('reset', ()async{});
 }
