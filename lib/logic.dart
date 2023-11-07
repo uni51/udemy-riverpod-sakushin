b0cbf9e988ee19039e946acd5f81c40feba4fd66
uni51 <unieye51@gmail.com>
2023-11-07 14:22:58 +0900

    25. Modelのロジックのクラスを作る

---
+++ lib/logic.dart
@@ -0,0 +1,31 @@
+import 'package:riverpod_counter/data/count_data.dart';
+
+class Logic {
+  CountData _countData = CountData(
+      count: 0,
+      countUp: 0,
+      countDown: 0
+  );
+
+  void increase() {
+    _countData = _countData.copyWith(
+        count: _countData.count + 1,
+        countUp: _countData.countUp + 1
+    );
+  }
+
+  void decrease() {
+    _countData = _countData.copyWith(
+        count: _countData.count - 1,
+        countDown: _countData.countDown + 1
+    );
+  }
+
+  void reset() {
+    _countData = CountData(
+        count: 0,
+        countUp: 0,
+        countDown: 0
+    );
+  }
+}
