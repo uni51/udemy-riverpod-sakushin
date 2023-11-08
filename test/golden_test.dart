ef2f028de69eb67aed7e92068d99e5e5974de77f
uni51 <unieye51@gmail.com>
2023-11-09 08:51:41 +0900

    36. GoldenTestでフォントを表示させる

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,12 +1,18 @@
 import 'dart:ui';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:riverpod_counter/main.dart';
 import 'package:riverpod_counter/view_model.dart';
 
 void main() {
+  setUpAll(() async{
+    await loadAppFonts();
+  });
+  
   testGoldens('normal', (tester) async {
-    const iPhone55 = Device(name: 'iPhone 5.5', size: Size(414, 736), devicePixelRatio: 3.0);
+    const iPhone55 =
+        Device(name: 'iPhone 5.5', size: Size(414, 736), devicePixelRatio: 3.0);
 
     List<Device> devices = [iPhone55];
 
@@ -14,8 +20,12 @@ void main() {
 
     await tester.pumpWidgetBuilder(
       ProviderScope(
-        child: MyHomePage(),
+        child: MyHomePage(
+          viewModel,
+        ),
       ),
     );
+
+    await multiScreenGolden(tester, 'myHomePage_0init', devices: devices);
   });
 }
