f55576314f8e0b6cca29ec9bfe6b1917db3d802f
uni51 <unieye51@gmail.com>
2023-11-11 13:53:12 +0900

    48. SoundLogicに音の鳴る条件を作成する

--- lib/logic/sound_logic.dart
+++ lib/logic/sound_logic.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:riverpod_counter/data/count_data.dart';
 
 
 class SoundLogic {
@@ -13,6 +14,16 @@ class SoundLogic {
 
   void load() {
     _cache.loadAll([SOUND_DATA_UP, SOUND_DATA_DOWN, SOUND_DATA_RESET]);
+  }
+
+  void valueChanged(CountData oldData, CountData newData) {
+    if (newData.countUp == 0 && newData.countDown == 0 && newData.count == 0) {
+      playResetSound();
+    } else if (oldData.countUp +1 == newData.countUp) {
+      playUpSound();
+    } else if (oldData.countDown +1 == newData.countDown) {
+      playDownSound();
+    }
   }
 
   void playUpSound() {
