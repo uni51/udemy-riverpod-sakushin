443feb4947da9e75f83d0a62a8d5a5f4d3bed936
uni51 <unieye51@gmail.com>
2023-11-11 13:49:40 +0900

    47. SoundLogicをViewModelから呼び出す

--- lib/view_model.dart
+++ lib/view_model.dart
@@ -1,13 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_counter/logic/logic.dart';
+import 'package:riverpod_counter/logic/sound_logic.dart';
 import 'package:riverpod_counter/provider.dart';
 
 class ViewModel {
   Logic _logic = Logic();
+
+  SoundLogic _soundLogic = SoundLogic();
+
   late WidgetRef _ref;
 
   void setRef(WidgetRef ref) {
     this._ref = ref;
+    _soundLogic.load();
   }
 
   get count => _ref.watch(countDataProvider).count.toString();
@@ -21,16 +26,22 @@ class ViewModel {
 
   void onIncrease() {
     _logic.increase();
+
     _ref.read(countDataProvider.state).state = _logic.countData;
+    _soundLogic.playUpSound();
   }
 
   void onDecrease() {
     _logic.decrease();
+
     _ref.read(countDataProvider.state).state = _logic.countData;
+    _soundLogic.playDownSound();
   }
 
   void onReset() {
     _logic.reset();
+
     _ref.read(countDataProvider.state).state = _logic.countData;
+    _soundLogic.playResetSound();
   }
 }
